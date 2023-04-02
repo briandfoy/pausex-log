2639f0c934d69987df9958cba65e67381b258731
brian d foy <briandfoy@pobox.com>
2023-04-01 23:55:32 -0400

    Initial import

---
+++ lib/PAUSEx/Log.pm
@@ -0,0 +1,65 @@
+package PAUSEx::Log;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+PAUSEx::Log - Access the PAUSE log
+
+=head1 SYNOPSIS
+
+	use PAUSEx::Log;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/pausex-log
+
+=head1 AUTHOR
+
+brian d foy, C<< <brian d foy> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2023, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
